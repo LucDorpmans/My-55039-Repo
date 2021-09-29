6ecde66f14a62ef10f0867fca21a418c74cfd67c
LucDorpmans <GitHub@sure.nl>
2021-09-30 00:10:10 +0200

    Upd

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -4,9 +4,9 @@ Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False,
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-55039-Repo/main/$AFile"  -Outfile "$SPath$AFile" 
 			If ($EditFile) { PSEdit  ("$SPath$AFile" )} }
 		
-Get-MyScript "EdgeMSI-Download-Only-Complete.ps1" -EditFile
-Get-MyScript "Edge-InstallOnly.ps1" -EditFile
-Get-MyScript "Chrome-Download+Run-Installer.ps1" -EditFile
+Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
+Get-MyScript "Edge-InstallOnly.ps1"
+Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "PowerShell-Core-7.1.4-Download+Install.ps1" -EditFile
 Get-MyScript "NuGet+PSGallery.ps1" -EditFile
 
--- NuGet+PSGallery.ps1
+++ NuGet+PSGallery.ps1
@@ -1,7 +1,9 @@
 # Unattended install of NuGet repository, and set PSGallery to be a Trusted repository
-If ((Get-PackageProvider -Name Nuget)) {
+$VerbosePreference = "Continue"
+
+If ((Get-PackageProvider -Name PowerShellGet -ListAvailable)) {
     $NuProv = Get-PackageProvider -Name Nuget
-    Write-Verbose "NuGet PackageProvider version $($NuProv.Version) installed and approved" 
+    Write-Verbose "NuGet PackageProvider version $($NuProv.Version) installed" 
 } 
 Else {
     Write-Verbose "Not installed, installing"
@@ -9,9 +11,14 @@ Else {
     Write-Verbose "Installed  (if no errors)"
 }
 
-If ( !(Get-PSRepository PSGallery).InstallationPolicy -eq "Trusted" )  {
+
+If ( (Get-PSRepository PSGallery).InstallationPolicy -eq "Trusted" )  {
+    Write-Verbose "PsGallery already trusted" } 
+Else { 
     Write-Verbose "{PsGallery not yet trusted, trying to approve"
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
- } 
-Else { Write-Verbose "PsGallery already trusted" }
+ }
+
+
+
 
