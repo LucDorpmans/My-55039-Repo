9c8c903c853c109e4f02de1ae08597eaac318ed9
LucDorpmans <GitHub@sure.nl>
2021-10-02 17:54:09 +0200

    Upd

---
+++ 55039-Install-Modules.ps1
@@ -0,0 +1,12 @@
+﻿# Install all Modules used in Course 55039 
+Install-module PSJsonCredential
+Install-Module PlatyPS
+Install-Module PSScriptAnalyzer
+
+Get-Module -ListAvailable P*
+
+Get-Command -module psjsoncredential
+
+
+# Install-Module Pester -Force -SkipPublisherCheck 
+
--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -13,4 +13,4 @@ Get-MyScript "NuGet+PSGallery.ps1" -EditFile
 
 # Get Install-VSCode script"
 Invoke-Webrequest -Uri "https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1" -Outfile "$env:USERPROFILE\Downloads\Install-VSCode.ps1"
-PSEdit  ("$env:USERPROFILE\Downloads\Install-VSCode.ps1")
+# PSEdit  ("$env:USERPROFILE\Downloads\Install-VSCode.ps1")
