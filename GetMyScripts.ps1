b7a329d9ddf1463d83bd130ebad96d4c26bb4bdb
LucDorpmans <GitHub@sure.nl>
2021-10-02 19:45:39 +0200

    Upd

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -4,14 +4,9 @@ Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False,
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-55039-Repo/main/$AFile"  -Outfile "$SPath$AFile" 
 			If ($EditFile) { PSEdit  ("$SPath$AFile" )} }
 		
-# Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
-# Get-MyScript "Edge-InstallOnly.ps1"
-# Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "PowerShell-Core-7.1.4-Download+Install.ps1" -EditFile
 Get-MyScript "55039-Install-Modules" -EditFile
 Get-MyScript "NuGet+PSGallery.ps1" -EditFile
-
-
 
 # Get Install-VSCode script"
 Invoke-Webrequest -Uri "https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1" -Outfile "$env:USERPROFILE\Downloads\Install-VSCode.ps1"
