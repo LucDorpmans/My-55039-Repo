da70151d3478382da635a2fbfe36e0f44aeb4a46
LucDorpmans <GitHub@sure.nl>
2022-09-06 13:33:52 +0200

    Upd

--- NuGet+PSGallery.ps1
+++ NuGet+PSGallery.ps1
@@ -1,4 +1,8 @@
 #[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" # For older Windows versions
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-PackageProvider nuget -Force
+# Install-PackageProvider nuget -Force
+If (!(Get-PackageProvider -Name Nuget -ListAvailable )) {
+   Write-Output "NuGet Not installed, installing"
+   Install-PackageProvider nuget -Force
+}
 Get-PackageProvider -Name Nuget
