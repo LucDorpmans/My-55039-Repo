7b0c226449c2940520744def286733393ae103ae
LucDorpmans <GitHub@sure.nl>
2022-11-09 23:00:10 +0100

    Update Config Scripts

---
+++ ConfigScripts/Edge-HideFirstRunExperience.ps1
@@ -0,0 +1,7 @@
+﻿# Hide Edge Chromium FirstRun experience
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Edge
+New-ItemProperty -Name HideFirstRunExperience -Path HKLM:\SOFTWARE\Policies\Microsoft\Edge -Value 1
+
+Get-ItemProperty -Name HideFirstRunExperience -Path HKLM:\SOFTWARE\Policies\Microsoft\Edge 
+Get-ItemPropertyValue -Name HideFirstRunExperience -Path HKLM:\SOFTWARE\Policies\Microsoft\Edge 
+
---
+++ ConfigScripts/GitHub-Desktop-Download+SilentInstall.ps1
@@ -0,0 +1,6 @@
+﻿# GitHub Desktop Silent Install (EXE)
+# Download the GitHubDesktopSetup-x64.exe 
+Invoke-WebRequest -uri "https://central.github.com/deployments/desktop/desktop/latest/win32" -OutFile $ENV:temp\GitHubDesktopSetup-x64.exe
+
+# Install Silently: 
+& $ENV:temp\GitHubDesktopSetup-x64.exe -s
