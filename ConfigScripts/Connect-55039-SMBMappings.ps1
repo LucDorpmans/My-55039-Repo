64f5f03d8fd566ccdac4343a3ce39b5d1618fc3d
LucDorpmans <GitHub@sure.nl>
2022-11-09 23:04:41 +0100

    Update

--- ConfigScripts/Connect-55039-SMBMappings.ps1
+++ ConfigScripts/Connect-55039-SMBMappings.ps1
@@ -1,9 +1,9 @@
 ﻿Get-SmbMapping | Remove-SmbMapping -Force
 
 $MyRepo = "My-55039-Repo-main"
-# New-SmbMapping -LocalPath x: -RemotePath \\LON-DC1\MyRepo\LabFiles.org\LON-CL1 
-New-SmbMapping -LocalPath E: -RemotePath \\LON-DC1\MyRepo\GK-Custom-LabFiles 
-New-SmbMapping -LocalPath F: -RemotePath \\LON-DC1\MyRepo
-# New-SmbMapping -LocalPath x: -RemotePath \\LON-DC1\MyRepo\My-55039-LabFiles
+# New-SmbMapping -LocalPath x: -RemotePath \\Alpha\MyRepo\LabFiles.org\LON-CL1 
+New-SmbMapping -LocalPath E: -RemotePath \\Alpha\MyRepo\GK-Custom-LabFiles 
+New-SmbMapping -LocalPath F: -RemotePath \\Alpha\MyRepo
+# New-SmbMapping -LocalPath x: -RemotePath \\Alpha\MyRepo\My-55039-LabFiles
 
 # Get-SmbMapping
